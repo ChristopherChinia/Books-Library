b8b9fdfebb429d015257b7e9765551097f8b99b1
christopherchinia <chinia214@gmail.com>
2022-07-26 22:27:59 -0400

    using useState hook to store book dataand useEffect hook to provide get accessability and passed in dependency array to call data once

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -1,8 +1,18 @@
-import React from 'react'
 import React,{ useState, useEffect} from 'react'
 import BookShelf from './BookShelf'
 
 const BookStore = () => {
+    // useState hook to store book data
+    const [books, displayBooks]=useState([])
+
+    // useEffect hook to provide get accessability
+    useEffect(() =>{
+        fetch("http://localhost:8001/Books")
+        .then((r) => r.json())
+            .then((data) => {
+              displayBooks(data);
+      })
+      },[]);
   return (
     <div>
         <h1>This is my bookstore component!</h1>
