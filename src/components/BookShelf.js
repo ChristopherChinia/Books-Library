76a6545e738c17a551e58d47e3a4715246ec7a70
christopherchinia <chinia214@gmail.com>
2022-07-26 23:45:28 -0400

    adding a div container book-box in booshelf file

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const BookShelf = ({ book }) => {
-    const { title, cover_image, releaseDate, author } = book;
+  const { title, cover_image, releaseDate, author,summary } = book;
   return (
-    <>
     <div className="book-shelf">
-      <div>
-        <p>{releaseDate}</p>
+      <div className="book-box">
+        <div>
+          <p>{releaseDate}</p>
+        </div>
+        <div>
+          <img
+            src={cover_image ? cover_image : "https://via.placeholder.com/400"}
+            alt={title}
+          />
+        </div>
+        <div>
+          <h3>{title}</h3>
+          <h3>- By {author}</h3>
+        </div>
       </div>
       <div>
-        <img
-          src={cover_image ? cover_image : "https://via.placeholder.com/400"}
-          alt={title}
-        />
       </div>
-      <div>
-        <h3>{title}</h3>
-        <h3>- By {author}</h3>
-      </div>
-      
     </div>
-  </>
-  )
-}
+  );
+};
 
-export default BookShelf
+export default BookShelf;
