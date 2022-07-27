ffac25fc42b8ddc837ace106f2197bbfc3933e2e
christopherchinia <chinia214@gmail.com>
2022-07-27 02:27:33 -0400

    adding a footer in the about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import image from './image2.jpeg';
+
 
 const About = () => {
   return (
-    <div>
+    <div className='about'>
+        <div className='flex'>
+            <img src={image} className='img-responsive' />
         <h1>This is my about component!</h1>
+        </div>
+        <footer>
+        <h1>Created By <a href="#">Christopher chinia</a> |  2022 All rights reserved.</h1>
+        </footer>
     </div>
   )
 }
